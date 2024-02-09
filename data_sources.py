8297412618c9d425adffc1b772587d107805111b
kesterlester <cgl20@cam.ac.uk>
2024-02-09 22:02:36 +0000

    numpy beginnings

--- data_sources.py
+++ data_sources.py
@@ -1,21 +1,25 @@
-#!/usr/bin/env python3
+# In general, data is a length-n mulitiset contianing "n" orderd lists, each of length-m.
+# However, if m=1, the data can just be a length-n multiset of values.
+# I.e. the following are OK:
+#
+# (m,n)=(1,4) : data=(1,2,3,4,)                  size=(4) or (1,4)
+# (m,n)=(1,4) : data=((1),(2),(3),(4),)          size=(4,1)
+# (m,n)=(2,4) : data=((1,2),(3,4),(5,6),(7,8) )  size=(4,2)
 
-# some random data that is a length-n list of lenth-m lists:
+
+import numpy as np
+
 def random_real_2D_data(mn=(1,1) ):
-    import random
     m,n = mn
-    return ([    list(           ( random.randrange(-10,10) for j in range(m))             )            for i in range(n)])
-    #return ([                    ( random.randrange(-10,10) for j in range(m))                          for i in range(n)])
+    return np.random.rabdint(low=-9,high=10,size=(n,m))
 
 # some random data that is a length-n list of reals
 def random_real_1D_data(n=1):
-    import random
-    return ( random.randrange(-10,10) for i in range(n) )
+    return np.random.randint(low=-9,high=10,size=n)
 
 # some random data that is a length-n list of complex numbers
 def random_complex_1D_data(n=1):
-    import random
-    return ( complex(random.randrange(-10,10), random.randrange(-10,10)) for i in range(n) )
+    return random_real_1D_data(n) + complex(0,1)*random_real_1D_data(n)
 
 
 
