9bad0dd2c738254f42b66e98591940d7b54d1561
kesterlester <cgl20@cam.ac.uk>
2024-02-09 20:48:43 +0000

    Factoring

--- test.py
+++ test.py
@@ -4,9 +4,7 @@ import Cinf_polynomial_encoder_for_list_of_real_or_complex_numbers as encoder_Ci
 import C0_sorting_encoder_for_list_of_real_numbers as encoder_C0
 import data_sources
 
-
-
-def test(data, encoder, encoder_name, number_of_shuffled_copies=3):
+def test_encoder(data, encoder, encoder_name, number_of_shuffled_copies=3):
     import random
     data = list(data)
     # shuffled_copies = [ random.shuffle(data.copy()) for i in range(number_of_shuffled_copies) ]
@@ -18,22 +16,24 @@ def test(data, encoder, encoder_name, number_of_shuffled_copies=3):
         print("ENCH ",encoder_name," is ",encoding)
     print()
 
-for i in range(10):
+def test_various_encoders():
 
-    test(
+    test_encoder(
        data=data_sources.random_real_1D_data(n=3),
        encoder=encoder_Cinf,
        encoder_name="Cinf",
     )
 
-    test(
+    test_encoder(
        data=data_sources.random_complex_1D_data(n=3),
        encoder=encoder_Cinf,
        encoder_name="Cinf",
     )
 
-    test(
+    test_encoder(
        data=data_sources.random_real_1D_data(n=3),
        encoder=encoder_C0,
        encoder_name="C0",
     )
+
+test_various_encoders()
