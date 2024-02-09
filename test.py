aebefa46e79e997a5fb588ed2a843405b7656f0f
kesterlester <cgl20@cam.ac.uk>
2024-02-09 22:16:26 +0000

    Numpy

--- test.py
+++ test.py
@@ -5,31 +5,29 @@ import C0_sorting_encoder_for_list_of_real_numbers as encoder_C0
 import data_sources
 
 def test_encoder(data, encoder, number_of_shuffled_copies=3):
-    import random
-    data=list(data)
-    # shuffled_copies = [ random.shuffle(data.copy()) for i in range(number_of_shuffled_copies) ]
-    shuffled_copies = [ random.sample(data, len(data)) for i in range(number_of_shuffled_copies) ]
-    print("DATA is",data)
-    for shuffled_data in shuffled_copies:
-        #print("SHUFF DATA is",shuffled_data)
-        encoding = list(encoder.encode(shuffled_data))
-        print("ENCH",encoder.name,"is",encoding)
+    import numpy
+    print("ORIGINAL DATA is",data)
+    shuffled_data = data.copy()
+    for i in range(number_of_shuffled_copies):
+        encoding = encoder.encode(shuffled_data)
+        print("ENCH",encoder.name,"generates",encoding,"from",shuffled_data)
+        numpy.random.shuffle(shuffled_data) 
     print()
 
 def test_various_encoders():
 
-    test_encoder(
-       data=data_sources.random_real_1D_data(n=3),
-       encoder=encoder_Cinf,
-    )
+    # test_encoder(
+    #    data=data_sources.random_real_1D_data(n=4),
+    #    encoder=encoder_Cinf,
+    # )
+
+    # test_encoder(
+    #    data=data_sources.random_complex_1D_data(n=4),
+    #    encoder=encoder_Cinf,
+    # )
 
     test_encoder(
-       data=data_sources.random_complex_1D_data(n=3),
-       encoder=encoder_Cinf,
-    )
-
-    test_encoder(
-       data=data_sources.random_real_1D_data(n=3),
+       data=data_sources.random_real_1D_data(n=4),
        encoder=encoder_C0,
     )
 
