cda2449e6926961f791e08a487cda42020ee0537
kesterlester <cgl20@cam.ac.uk>
2024-02-10 09:44:30 +0000

    NP

--- Cinf_np_polynomial_encoder_for_list_of_real_or_complex_numbers.py
+++ Cinf_np_polynomial_encoder_for_list_of_real_or_complex_numbers.py
@@ -13,4 +13,4 @@ name="Cinf_np_polynomial_encoder_for_list_of_real_or_complex_numbers"
 from numpy import polynomial
 
 def encode(data):
-    return polynomial.polynomial.polyfromroots(data)
+    return polynomial.polynomial.polyfromroots(data)[-2::-1]
