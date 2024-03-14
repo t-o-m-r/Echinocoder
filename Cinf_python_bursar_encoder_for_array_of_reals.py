b4f213832384244aea7f661fabe6bd081e1cfb6e
kesterlester <cgl20@cam.ac.uk>
2024-03-14 12:17:48 +0000

    Update

--- Cinf_python_bursar_encoder_for_array_of_reals.py
+++ Cinf_python_bursar_encoder_for_array_of_reals.py
@@ -8,21 +8,48 @@
 name="Cinf_python_bursar_encoder_for_array_of_reals"
 
 from math import prod
-from itertools import combinations
-import numpy as np
+#from itertools import combinations
+#import numpy as np
 #import tools
-
+from sympy import Poly, abc
 
 def encode(data):
     
     print("Data is")
     print(data)
-    xPolys = [ np.polynomial.Polynomial(vector) for vector in data ] 
-    print("x polynomials are: ")
-    for xPoly in xPolys:
-        print (xPoly)
+
+    n = len(data)
+    if n==0:
+        return []
+
+    m = len(data[0])
+    if m==0:
+        return []
+
+    polys = [ Poly(vector, abc.x)+Poly(abc.y) for vector in data ]
+
+    print("polys",polys)
+
+    product = prod(polys)
+    print ("Product",product)
+
+    # extract coeffs from product
+    for c in range(1,n+1):
+         yPower = n-c
+         for xPower in range(c*(m-1)+1):
+             coeff = product.coeff_monomial((abc.x)**xPower * (abc.y)**yPower)
+             print(coeff)
+
+    #xPolys = [ np.polynomial.Polynomial(vector) for vector in data ] 
+    #print("x polynomials are: ")
+    #for xPoly in xPolys:
+    #    print (xPoly)
+    # 
+    #n=len(data)
+
+    return [1,2,3]
     
-    return [ sum( [ prod(c)  for c in combinations(data, r+1) ] ) for r in range(len(data)) ]
+    #return [ sum( [ prod(c)  for c in combinations(data, r+1) ] ) for r in range(len(data)) ]
 
     # return [ sum( [ prod(c)  for c in combinations(data, r+1) ] ) for r in range(len(data)) ]
     
