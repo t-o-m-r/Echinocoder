d8a58831071f4f20a9e23bd94f8592e38bd10578
kesterlester <cgl20@cam.ac.uk>
2024-11-18 11:09:32 +0000

    Undoing backgrounding of bokeh

--- visualiser_start.sh
+++ visualiser_start.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-echo Before killing servers:
-ps -ax | grep -i bok
+## echo Before killing servers:
+## ps -ax | grep -i bok
+## 
+## # Kill existing bokeh server(s)
+## kill -9 `ps -waxf | grep bokeh | grep -v grep | awk '{print $2}'`
+## 
+## echo After killing servers:
+## ps -ax | grep -i bok
 
-# Kill existing bokeh server(s)
-kill -9 `ps -waxf | grep bokeh | grep -v grep | awk '{print $2}'`
-
-echo After killing servers:
-ps -ax | grep -i bok
+open http://localhost:5006/visualise
 
 echo Starting server:
-bokeh serve visualise.py &
+bokeh serve visualise.py 
 
-sleep 2
-open http://localhost:5006/visualise
 
