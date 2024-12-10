c4a912cb61b3f31eeebeb2fafa8b02a7328baa0e
kesterlester <cgl20@cam.ac.uk>
2024-12-10 22:50:11 +0000

    doc changes

--- MultisetEncoder.py
+++ MultisetEncoder.py
@@ -7,12 +7,12 @@ class MultisetEncoder:
     Encoders are not necessarily embedders since encoders do not need to be injective.
     All embedders are encoders, however.
 
-    Strictly speaking these are "multisets" not "sets" since the sets can hold repeated objects
+    Strictly speaking wecencode "multisets" not "sets" since the containers can hold repeated objects
     and retain knowledge of the number of repeats.  However, we are sometimes guity of 
     abbreviating "multiset" to just "set".
 
-    The set to be encode should be inputs as a 2D numpy array with shape (n,k).
-    The order of the vectors within the numpy array can be arbitrary
+    The set to be encoded should be inputs as a 2D numpy array with shape (n,k).
+    The order of the vectors within the numpy array can be arbitrary.
     E.g. to encode a multiset containing the 2-vectors (2,2), (4,5) and (1,2) one could call
     
         encode(np.asarray([[2,2], [4,5], [1,2]]))
@@ -22,14 +22,19 @@ class MultisetEncoder:
         encode(np.asarray([[4,5], [2,2], [1,2]]))
 
     and both should have the same output -- at least up to numerical precision. This leeway (permission to 
-    have small deviations on account of floating point precision, rather than demanding bit-for-bit identical embeddings) is granted to implementations in order to allow them to be faster (sometimes) than would be the case if they were all required to canonicalise their input sets.  Someone wanting bit-for-bit identical output under permutations of input vectors could easily sort their vectors (in any way) prior to using any encoder.
+    have small deviations on account of floating point precision, rather than demanding bit-for-bit identical
+    embeddings) is granted to implementations in order to allow them to be faster (sometimes) than would be the
+    case if they were all required to canonicalise their input sets.  Someone wanting bit-for-bit identical 
+    output under permutations of input vectors could easily sort their vectors (in any way) prior to using any 
+    encoder.
 
     All encoders return one dimensional arrays of real floats.
 
     In principle, a given encoder can embed sets of different sizes n and or k.  However, 
     some encoders might wish to restrict themselves to certain fixed n or k at initialisation (e.g. if 
     an embedder were to need a significant amount of n and k dependent set-up cost that it wished to do
-    once only).  Thus all encoders are expected to be able to tell callers what sizes of input they can and cannot embed, and how long the possible embeddings will be.  Derived classes do this by implementing the method
+    once only).  Thus all encoders are expected to be able to tell callers what sizes of input they can and 
+    cannot embed, and how long the possible embeddings will be.  Derived classes do this by implementing the method
 
         size_from_n_k(n: int, k, int) -> int:
 
