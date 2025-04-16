b5d8554a01f9080f30f6d6a64af498cc2a4ff6ca
kesterlester <cgl20@cam.ac.uk>
2025-04-16 13:29:10 +0100

    Rename

--- nice.sh
+++ nice.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-#./play_simplex1_EncDec.py array "[[17, 4, 28, ],  [10, 14, -7, ],  [15, 28, -8, ],  [26, 23, -2, ],  ]"
+#./play_simplex1_via_EncDec.py array "[[17, 4, 28, ],  [10, 14, -7, ],  [15, 28, -8, ],  [26, 23, -2, ],  ]"
 
-# ./play_simplex1_EncDec.py array "[[12, 29, 27, ],  [6, -8, 0, ],  [29, 9, 18, ],  [29, 15, 6, ],  ]"
+# ./play_simplex1_via_EncDec.py array "[[12, 29, 27, ],  [6, -8, 0, ],  [29, 9, 18, ],  [29, 15, 6, ],  ]"
 
-#./play_simplex1_EncDec.py array "[[709, 537, 355, ],  [725, 534, 223, ],  ]"
+#./play_simplex1_via_EncDec.py array "[[709, 537, 355, ],  [725, 534, 223, ],  ]"
 
-./play_simplex1_EncDec.py array "[[5,4,2 ], [6,3,1] ]"
+./play_simplex1_via_EncDec.py array "[[5,4,2 ], [6,3,1] ]"
