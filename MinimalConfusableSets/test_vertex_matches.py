433fccef713b54f21cf8cab8236bd6cc78912965
kesterlester <cgl20@cam.ac.uk>
2025-08-07 16:58:14 +0100

    Update

--- MinimalConfusableSets/test_vertex_matches.py
+++ MinimalConfusableSets/test_vertex_matches.py
@@ -6,7 +6,33 @@ import vertex_matches
 
 
 
-k4M4_all_canonical_matches_expected = [
+M4_all_canonical_matches_expected = [
+     ( 0, 0, 0,-1,),
+     ( 0, 0,-1, 0,),
+     ( 0,-1, 0, 0,),
+     (-1, 0, 0, 0,),
+
+     ( 0, 1, 1,-1,),
+     ( 0, 1,-1 ,1,),
+     ( 0,-1, 1, 1,),
+     ( 1, 0, 1,-1,),
+     ( 1, 0,-1 ,1,),
+     (-1, 0, 1, 1,),
+     ( 1, 1, 0,-1,),
+     ( 1,-1, 0, 1,),
+     (-1, 1, 0, 1,),
+     ( 1, 1,-1, 0,),
+     ( 1,-1 ,1, 0,),
+     (-1, 1, 1, 0,),
+
+     ( 0,-1,-1,-1,),
+     (-1, 0,-1,-1,),
+     (-1,-1, 0,-1,),
+     (-1,-1,-1, 0,),
+
+    ]
+
+k4M4_all_useful_canonical_matches_expected = [
      ( 0, 0, 0,-1,),
      ( 0, 0,-1, 0,),
      ( 0,-1, 0, 0,),
@@ -34,18 +60,19 @@ k4M4_all_canonical_matches_expected = [
 
 def test():
 
-    k4M4_all_canonical_matches_computed = list(vertex_matches.generate_all_canonical_matches(
-        k=2, # k=dimension of space
-        M=4, #number of bad bats
-        ))
+    test_programme = [
+        (2, 4, vertex_matches.generate_all_canonical_matches, M4_all_canonical_matches_expected, "M4 all",)
+        ]
 
-    LHS = sorted(k4M4_all_canonical_matches_computed)
-    RHS = sorted(k4M4_all_canonical_matches_expected)
+    for k, M, gen, expected, name in test_programme:
+  
+        LHS = sorted(gen(k=k, M=M))
+        RHS = sorted(expected)
 
-    print("k4M4_all_canonical_matches LHS and RHS were")
-    for i,j in zip(LHS,RHS):
-        print(i, j)
-    print()
+        print("Test '",name,"' has LHS and RHS:")
+        for i,j in zip(LHS,RHS):
+            print(i, j)
+        print()
 
-    assert LHS==RHS
+        assert LHS==RHS
 
