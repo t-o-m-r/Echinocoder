dcdfac9f526ba5882021a4d14d49a0a6a976ebea
kesterlester <cgl20@cam.ac.uk>
2025-08-08 00:57:47 +0100

    New features but need unit tests.

--- MinimalConfusableSets/vertex_matches.py
+++ MinimalConfusableSets/vertex_matches.py
@@ -2,9 +2,42 @@
 import math
 from distinct_permutations import distinct_permutations
 
-# Canonical matches have an even number of +1 and and odd number of -1 entries, and others zero.
+"""
+Canonical matches have an even number of +1 and and odd number of -1 entries, and others zero.
 
-# "Useful" canonical matches have at least k+1 non-zero entries (because all sums of <=k linearly dependent non-zero things in k-dimes are non-zero).
+"Useful" canonical matches have at least k+1 non-zero entries (because all sums of <=k linearly dependent non-zero things in k-dimes are non-zero).
+
+Sometimes it is not worth permuting canonical matches over every bad bad because other matches in the existing context may not yet have broken any symmetries between the bats.  WLOG we choose to put the entries which are deemed to have already broen symmetry FIRST.  And the argument "fixed_places" describes how many such spaces there are.
+E.g.: fixed_places=2 means that the first two position (i.e. pos 0 and pos 1) have broken some symmetry and permutations on them are relevant, whereas permutations on pos 2 and above are irrelevant.
+E.g. if one listed all distinct permutations of the letters "Speedo" for "fixed_places=2" one would want these (in which the "." is not actually part of the string but has been inserted to help guide the eye so that you can see that only the first two letters explore all perms wile after the dot there is no perming.  Note that there are 4*4+5 = 21 of these:
+
+    Sp.eedo
+    Se.pedo
+    Sd.peeo
+    So.peed
+
+    pS.eedo
+    pe.Sedo
+    pd.Seeo
+    po.Seed
+
+    eS.pedo
+    ep.pedo
+    ee.pedo  # Yes, this group has five on account of the double "e" !
+    ed.pedo
+    eo.Sped
+
+    dS.peeo
+    dp.Seep
+    de.Speo
+    do.Spee
+
+    oS.peed
+    op.Seed
+    oe.Sped
+    od.Spee
+"""
+
 
 def smallest_odd_number_greater_than_or_equal_to(x):
   return 2*math.ceil((x+1)/2)-1 
@@ -55,14 +88,75 @@ def generate_all_canonical_matches(
                 # if show_only_useful_matches and (number_of_ones + number_of_minus_ones <= k):
                 #      continue
                 minus_ones = (-1,)*number_of_minus_ones
-                number_of_zeroes= M-number_of_ones-number_of_minus_ones
-                zeros = (0,)*number_of_zeroes
+                number_of_zeros= M-number_of_ones-number_of_minus_ones
+                zeros = (0,)*number_of_zeros
 
                 if permute:
                     for match in distinct_permutations( ones + minus_ones + zeros):
                         yield match
                 else:
                     yield ones + minus_ones + zeros
+
+def generate_all_useful_matches_given_fixed_places(
+        k, # k=dimension of space
+        M, #number of bad bats
+        fixed_places = 0, # permutations take place within the first "fixed_places" places, otherwise not.
+        ):
+    yield from generate_all_matches_given_fixed_places(k=k, M=M, show_only_useful_matches = True, fixed_places=fixed_places)
+    
+def generate_all_matches_given_fixed_places(
+        k, # k=dimension of space
+        M, #number of bad bats
+        show_only_useful_matches = False,
+        fixed_places = 0, # permutations take place within the first "fixed_places" places, otherwise not.
+        ):
+
+        if M<0:
+            raise ValueError(f"M should be a non-negative integer but is {M}.")
+
+        if fixed_places<0 or fixed_places>M:
+            raise ValueError(f"fixed_places should be in [0,{M}]  but is {fixed_places}.")
+
+        for number_of_ones in range(0, M+1, 2):
+            #ones = (1,)*number_of_ones
+
+            if show_only_useful_matches:
+                # In this case we need 
+                #         number_of_ones + number_of_minus_ones > k  and   number_of_minus_ones >= 1
+                # so
+                #         number_of_minus_ones > k - number_of_ones   and number_of_minus_ones >= 1
+                # so
+                #         number_of_minus_ones >= k - number_of_ones + 1 and number_of_minus_ones >=1
+                # so
+                #         number_of_minus_ones >= max(k - number_of_ones + 1, 1)
+                start_for_number_of_minus_ones =  max(1, smallest_odd_number_greater_than_or_equal_to(k - number_of_ones + 1))
+            else:
+                start_for_number_of_minus_ones = 1
+            for number_of_minus_ones in range(start_for_number_of_minus_ones, M+1-number_of_ones, 2):
+                # In alternative (but slower) implementation, one could always have start_for_number_of_minus_ones=1 but then 
+                # uncomment the next two lines:
+                # if show_only_useful_matches and (number_of_ones + number_of_minus_ones <= k):
+                #      continue
+                #minus_ones = (-1,)*number_of_minus_ones
+                number_of_zeros= M-number_of_ones-number_of_minus_ones
+                #zeros = (0,)*number_of_zeros
+
+                for perming_ones, non_perming_ones in bi_range(number_of_ones):
+                    if perming_ones > fixed_places:
+                        break
+                    for perming_minus_ones, non_perming_minus_ones in bi_range(number_of_minus_ones):
+                        if perming_ones + perming_minus_ones > fixed_places:
+                            break
+                        perming_zeros = fixed_places - (perming_ones + perming_minus_ones) # This is guaranteed non-negative
+                        non_perming_zeros = number_of_zeros - perming_zeros # this could be negative (but should not be) as we are not (yet) being careful enough with the ranges for perming ones, etc. So:
+                        if non_perming_zeros <0:
+                            continue # SLOW, SILLY TODO IMPROVE!
+
+                        perming_part = (1,)*perming_ones + (-1,)*perming_minus_ones + (0,)*perming_zeros
+                        non_perming_part = (1,)*non_perming_ones + (-1,)*non_perming_minus_ones + (0,)*non_perming_zeros
+
+                        for perm in distinct_permutations(perming_part):
+                            yield perm + non_perming_part
 
 
 if __name__ == "__main__":
@@ -71,16 +165,31 @@ if __name__ == "__main__":
     for i, match in enumerate(generate_all_canonical_matches(k=None, M=M)):
        print(f"   {i+1}:    {match}")
     print()
+
     k=2
     print(f"All USEFUL matches in k={k} dimensions, given M={M} bad bats are:")
     for i,match in enumerate(generate_all_useful_canonical_matches(k=k, M=M)):
        print(f"   {i+1}:    {match}")
     print()
+
     print(f"All matches in k={k} dimensions, given M={M} bad bats, but ignoring permutations are:")
     for i,match in enumerate(generate_all_canonical_matches(k=k, M=M, permute=False)):
        print(f"   {i+1}:    {match}")
     print()
+
     print(f"All USEFUL matches in k={k} dimensions, given M={M} bad bats, but ignoring permutations are:")
     for i,match in enumerate(generate_all_useful_canonical_matches(k=k, M=M, permute=False)):
        print(f"   {i+1}:    {match}")
     print()
+
+    for fixed_places in range(5):
+        print(f"All matches in k={k} dimensions, given M={M} bad bats, for fixed_places={fixed_places}")
+        for i,match in enumerate(generate_all_matches_given_fixed_places(k=k, M=M, fixed_places=fixed_places)):
+           print(f"   {i+1}:    {match}")
+        print()
+
+    for fixed_places in range(5):
+        print(f"All USEFUL matches in k={k} dimensions, given M={M} bad bats, for fixed_places={fixed_places}")
+        for i,match in enumerate(generate_all_useful_matches_given_fixed_places(k=k, M=M, fixed_places=fixed_places)):
+           print(f"   {i+1}:    {match}")
+        print()
