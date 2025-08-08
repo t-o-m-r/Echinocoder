e84accc5a12162f4e44b569594a8a442be7a7a26
kesterlester <cgl20@cam.ac.uk>
2025-08-08 02:57:38 +0100

    Better naming

--- MinimalConfusableSets/test_vertex_matches.py
+++ MinimalConfusableSets/test_vertex_matches.py
@@ -86,7 +86,7 @@ k2M4_all_useful_canonical_matches_expected = [
      (-1,-1, 0,-1,),
      (-1,-1,-1, 0,),
     ]
-k2M4_all_useful_canonical_matches_with_1_fixed_places = [
+k2M4_all_useful_canonical_matches_with_1_perming_places = [
      #( 0, 0, 0,-1,),
      #( 0, 0,-1, 0,),
      #( 0,-1, 0, 0,),
@@ -110,7 +110,7 @@ k2M4_all_useful_canonical_matches_with_1_fixed_places = [
      #(-1,-1, 0,-1,),
      (-1,-1,-1, 0,),
 ]
-k2M4_all_useful_canonical_matches_with_2_fixed_places = [
+k2M4_all_useful_canonical_matches_with_2_perming_places = [
      #( 0, 0, 0,-1,),
      #( 0, 0,-1, 0,),
      #( 0,-1, 0, 0,),
@@ -160,14 +160,14 @@ k3M4_all_useful_canonical_matches_expected = [
     ]
 
 """
-All USEFUL matches in k=2 dimensions, given M=4 bad bats, for fixed_places=1
+All USEFUL matches in k=2 dimensions, given M=4 bad bats, for perming_places=1
    1:    (0, -1, -1, -1)
    2:    (-1, -1, -1, 0)
    3:    (0, 1, 1, -1)
    4:    (-1, 1, 1, 0)
    5:    (1, 1, -1, 0)
 
-All USEFUL matches in k=2 dimensions, given M=4 bad bats, for fixed_places=2
+All USEFUL matches in k=2 dimensions, given M=4 bad bats, for perming_places=2
    1:    (-1, 0, -1, -1)
    2:    (0, -1, -1, -1)
    3:    (-1, -1, -1, 0)
@@ -252,8 +252,8 @@ def test_helper_functions():
 def test_main_generators():
 
 
-    def set_fixed_places(f, fixed_places=0):
-        return lambda k, M: f(k=k, M=M, fixed_places=fixed_places)
+    def set_perming_places(f, perming_places=0):
+        return lambda k, M: f(k=k, M=M, perming_places=perming_places)
         
     test_programme = [
         (None, 0, vertex_matches.generate_all_canonical_matches, M0_all_canonical_matches_expected, "M0 all",),
@@ -265,17 +265,17 @@ def test_main_generators():
         (2, 4, vertex_matches.generate_all_useful_canonical_matches, k2M4_all_useful_canonical_matches_expected, "k2M4 useful",),
         (3, 4, vertex_matches.generate_all_useful_canonical_matches, k3M4_all_useful_canonical_matches_expected, "k3M4 useful",),
 
-        (2, 4, set_fixed_places(vertex_matches.generate_all_useful_matches_given_fixed_places, fixed_places=4), k2M4_all_useful_canonical_matches_expected, "k2M4 useful but testing fixed_places=4}",),
-        (2, 4, set_fixed_places(vertex_matches.generate_all_useful_matches_given_fixed_places, fixed_places=4), list(vertex_matches.generate_all_useful_canonical_matches(k=2, M=4, permute=True)), "k2M4 useful but testing fixed_places=4}",), # permute=True is like fixed_places=M
-        (2, 4, set_fixed_places(vertex_matches.generate_all_useful_matches_given_fixed_places, fixed_places=3), list(vertex_matches.generate_all_useful_canonical_matches(k=2, M=4, permute=True)), "k2M4 useful but testing fixed_places=3}",), # permute=True is also like fixed_places=M-1
-        (2, 4, set_fixed_places(vertex_matches.generate_all_useful_matches_given_fixed_places, fixed_places=2), k2M4_all_useful_canonical_matches_with_2_fixed_places, "k2M4 useful but testing fixed_places=2}",),
-        (2, 4, set_fixed_places(vertex_matches.generate_all_useful_matches_given_fixed_places, fixed_places=1), k2M4_all_useful_canonical_matches_with_1_fixed_places, "k2M4 useful but testing fixed_places=1}",),
-        (2, 4, set_fixed_places(vertex_matches.generate_all_useful_matches_given_fixed_places, fixed_places=0), list(vertex_matches.generate_all_useful_canonical_matches(k=2, M=4, permute=False)), "k2M4 useful but testing fixed_places=0}",),
+        (2, 4, set_perming_places(vertex_matches.generate_all_useful_matches_given_perming_places, perming_places=4), k2M4_all_useful_canonical_matches_expected, "k2M4 useful but testing perming_places=4}",),
+        (2, 4, set_perming_places(vertex_matches.generate_all_useful_matches_given_perming_places, perming_places=4), list(vertex_matches.generate_all_useful_canonical_matches(k=2, M=4, permute=True)), "k2M4 useful but testing perming_places=4}",), # permute=True is like perming_places=M
+        (2, 4, set_perming_places(vertex_matches.generate_all_useful_matches_given_perming_places, perming_places=3), list(vertex_matches.generate_all_useful_canonical_matches(k=2, M=4, permute=True)), "k2M4 useful but testing perming_places=3}",), # permute=True is also like perming_places=M-1
+        (2, 4, set_perming_places(vertex_matches.generate_all_useful_matches_given_perming_places, perming_places=2), k2M4_all_useful_canonical_matches_with_2_perming_places, "k2M4 useful but testing perming_places=2}",),
+        (2, 4, set_perming_places(vertex_matches.generate_all_useful_matches_given_perming_places, perming_places=1), k2M4_all_useful_canonical_matches_with_1_perming_places, "k2M4 useful but testing perming_places=1}",),
+        (2, 4, set_perming_places(vertex_matches.generate_all_useful_matches_given_perming_places, perming_places=0), list(vertex_matches.generate_all_useful_canonical_matches(k=2, M=4, permute=False)), "k2M4 useful but testing perming_places=0}",),
 
-        (2, 4, set_fixed_places(vertex_matches.generate_all_matches_given_fixed_places, fixed_places=4), M4_all_canonical_matches_expected, "M4 but testing fixed_places=4}",),
-        (2, 4, set_fixed_places(vertex_matches.generate_all_matches_given_fixed_places, fixed_places=4), list(vertex_matches.generate_all_canonical_matches(k=2, M=4, permute=True)), "M4 but testing fixed_places=4}",), # permute=True is like fixed_places=M
-        (2, 4, set_fixed_places(vertex_matches.generate_all_matches_given_fixed_places, fixed_places=3), list(vertex_matches.generate_all_canonical_matches(k=2, M=4, permute=True)), "M4 but testing fixed_places=3}",), # permute=True is also like fixed_places=M-1
-        (2, 4, set_fixed_places(vertex_matches.generate_all_matches_given_fixed_places, fixed_places=0), list(vertex_matches.generate_all_canonical_matches(k=2, M=4, permute=False)), "M4 but testing fixed_places=0}",),
+        (2, 4, set_perming_places(vertex_matches.generate_all_matches_given_perming_places, perming_places=4), M4_all_canonical_matches_expected, "M4 but testing perming_places=4}",),
+        (2, 4, set_perming_places(vertex_matches.generate_all_matches_given_perming_places, perming_places=4), list(vertex_matches.generate_all_canonical_matches(k=2, M=4, permute=True)), "M4 but testing perming_places=4}",), # permute=True is like perming_places=M
+        (2, 4, set_perming_places(vertex_matches.generate_all_matches_given_perming_places, perming_places=3), list(vertex_matches.generate_all_canonical_matches(k=2, M=4, permute=True)), "M4 but testing perming_places=3}",), # permute=True is also like perming_places=M-1
+        (2, 4, set_perming_places(vertex_matches.generate_all_matches_given_perming_places, perming_places=0), list(vertex_matches.generate_all_canonical_matches(k=2, M=4, permute=False)), "M4 but testing perming_places=0}",),
 
         ]
 
--- MinimalConfusableSets/vertex_matches.py
+++ MinimalConfusableSets/vertex_matches.py
@@ -7,9 +7,9 @@ Canonical matches have an even number of +1 and and odd number of -1 entries, an
 
 "Useful" canonical matches have at least k+1 non-zero entries (because all sums of <=k linearly dependent non-zero things in k-dimes are non-zero).
 
-Sometimes it is not worth permuting canonical matches over every bad bad because other matches in the existing context may not yet have broken any symmetries between the bats.  WLOG we choose to put the entries which are deemed to have already broen symmetry FIRST.  And the argument "fixed_places" describes how many such spaces there are.
-E.g.: fixed_places=2 means that the first two position (i.e. pos 0 and pos 1) have broken some symmetry and permutations on them are relevant, whereas permutations on pos 2 and above are irrelevant.
-E.g. if one listed all distinct permutations of the letters "Speedo" for "fixed_places=2" one would want these (in which the "." is not actually part of the string but has been inserted to help guide the eye so that you can see that only the first two letters explore all perms wile after the dot there is no perming.  Note that there are 4*4+5 = 21 of these:
+Sometimes it is not worth permuting canonical matches over every bad bat because other matches in the existing context may not yet have broken any symmetries between the bats.  WLOG we choose to put the entries which are deemed to have already broken symmetry FIRST.  And the argument "perming_places" describes how many such spaces there are.
+E.g.: perming_places=2 means that the first two position (i.e. pos 0 and pos 1) have broken some symmetry and permutations on them are relevant, whereas permutations on pos 2 and above are irrelevant.
+E.g. if one listed all distinct permutations of the letters "Speedo" for "perming_places=2" one would want these (in which the "." is not actually part of the string but has been inserted to help guide the eye so that you can see that only the first two letters explore all perms wile after the dot there is no perming.  Note that there are 4*4+5 = 21 of these:
 
     Sp.eedo
     Se.pedo
@@ -161,27 +161,27 @@ def generate_all_canonical_matches(
                 else:
                     yield ones + minus_ones + zeros
 
-def generate_all_useful_matches_given_fixed_places(
+def generate_all_useful_matches_given_perming_places(
         k, # k=dimension of space
         M, # number of bad bats
-        fixed_places = 0, # permutations take place within the first "fixed_places" places, otherwise not.
+        perming_places = 0, # permutations take place within the first "perming_places" places, otherwise not.
         ):
-    yield from generate_all_matches_given_fixed_places(k=k, M=M, show_only_useful_matches = True, fixed_places=fixed_places)
+    yield from generate_all_matches_given_perming_places(k=k, M=M, show_only_useful_matches = True, perming_places=perming_places)
     
-def generate_all_matches_given_fixed_places(
+def generate_all_matches_given_perming_places(
         k, # k=dimension of space
         M, # number of bad bats
         show_only_useful_matches = False,
-        fixed_places = 0, # permutations take place within the first "fixed_places" places, otherwise not.
+        perming_places = 0, # permutations take place within the first "perming_places" places, otherwise not.
         ):
 
         if M<0:
             raise ValueError(f"M should be a non-negative integer but is {M}.")
 
-        if fixed_places<0 or fixed_places>M:
-            raise ValueError(f"fixed_places should be in [0,{M}]  but is {fixed_places}.")
+        if perming_places<0 or perming_places>M:
+            raise ValueError(f"perming_places should be in [0,{M}]  but is {perming_places}.")
 
-        non_fixed_places = M-fixed_places
+        non_perming_places = M-perming_places
 
         for number_of_ones in range(0, M+1, 2):
             #ones = (1,)*number_of_ones
@@ -207,9 +207,9 @@ def generate_all_matches_given_fixed_places(
                 number_of_zeros= M-number_of_ones-number_of_minus_ones
                 #zeros = (0,)*number_of_zeros
 
-                for perming_ones, non_perming_ones in bi_range_with_maxes(number_of_ones, max_first=fixed_places, max_second=non_fixed_places):
-                    for perming_minus_ones, non_perming_minus_ones in bi_range_with_maxes(number_of_minus_ones, max_first = fixed_places-perming_ones, max_second=non_fixed_places - non_perming_ones):
-                        perming_zeros = fixed_places - (perming_ones + perming_minus_ones)
+                for perming_ones, non_perming_ones in bi_range_with_maxes(number_of_ones, max_first=perming_places, max_second=non_perming_places):
+                    for perming_minus_ones, non_perming_minus_ones in bi_range_with_maxes(number_of_minus_ones, max_first = perming_places-perming_ones, max_second=non_perming_places - non_perming_ones):
+                        perming_zeros = perming_places - (perming_ones + perming_minus_ones)
                         non_perming_zeros = number_of_zeros - perming_zeros
 
                         assert perming_zeros >=0
@@ -245,14 +245,14 @@ if __name__ == "__main__":
        print(f"   {i+1}:    {match}")
     print()
 
-    for fixed_places in range(5):
-        print(f"All matches in k={k} dimensions, given M={M} bad bats, for fixed_places={fixed_places}")
-        for i,match in enumerate(generate_all_matches_given_fixed_places(k=k, M=M, fixed_places=fixed_places)):
+    for perming_places in range(5):
+        print(f"All matches in k={k} dimensions, given M={M} bad bats, for perming_places={perming_places}")
+        for i,match in enumerate(generate_all_matches_given_perming_places(k=k, M=M, perming_places=perming_places)):
            print(f"   {i+1}:    {match}")
         print()
 
-    for fixed_places in range(5):
-        print(f"All USEFUL matches in k={k} dimensions, given M={M} bad bats, for fixed_places={fixed_places}")
-        for i,match in enumerate(generate_all_useful_matches_given_fixed_places(k=k, M=M, fixed_places=fixed_places)):
+    for perming_places in range(5):
+        print(f"All USEFUL matches in k={k} dimensions, given M={M} bad bats, for perming_places={perming_places}")
+        for i,match in enumerate(generate_all_useful_matches_given_perming_places(k=k, M=M, perming_places=perming_places)):
            print(f"   {i+1}:    {match}")
         print()
