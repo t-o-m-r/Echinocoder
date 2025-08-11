83f17df6f0f15525c53857856e6eff2e0f9c2790
kesterlester <cgl20@cam.ac.uk>
2025-08-11 12:48:15 +0100

    Factoring out bi_range

--- MinimalConfusableSets/vertex_matches.py
+++ MinimalConfusableSets/vertex_matches.py
@@ -1,6 +1,7 @@
 #import itertools
 import math
 from distinct_permutations import distinct_permutations
+from bi_range import bi_range_with_maxes
 
 """
 Vertex matches have an even number of +1 and and odd number of -1 entries, and others zero. Their total number of entries is M, the numnber of bad bats.
@@ -43,80 +44,6 @@ E.g. if one listed all distinct permutations of the letters "Speedo" for "permin
 
 def smallest_odd_number_greater_than_or_equal_to(x):
   return 2*math.ceil((x+1)/2)-1 
-
-def bi_range(n):
-    """
-    Whereas range(n) iterates over 0,1,2,3,...,n-1 ... bi_range(n) iterates over non-negative pairs of integers which sum to n.
-    The first integer grows, while the second integer shrinks.
-    For example, range(4) would iterate over
-        (0,4), (1,3), (2,2), (3,1), (4,0).
-    """
-    for i in range(n+1):
-        yield i, n-i
-
-def bi_range_with_maxes_crude(n, max_first, max_second):
-    """
-    Whereas range(n) iterates over 0,1,2,3,...,n-1 ... bi_range(n) iterates over non-negative pairs of integers which sum to n.
-    The first integer grows, while the second integer shrinks.
-    The first integer may not exceed max_first, while the second may not exceed max_second.
-    For example, range(4, 2,3) would iterate over
-         (1,3), (2,2)
-    which starts at the max_second contraint, and goes to the max_first constraint.
-    If the sum of both maxes is less than the total n, there will be no valid iterator positions. So:
-    """
-   
-    for i in range(n+1):
-       if i>max_first or n-i>max_second:
-           continue # TODO! Slow Fix!
-       yield i, n-i
-
-def bi_range_with_maxes(n, max_first, max_second):
-    """
-    Whereas range(n) iterates over 0,1,2,3,...,n-1 ... bi_range(n) iterates over non-negative pairs of integers which sum to n.
-    The first integer grows, while the second integer shrinks.
-    The first integer may not exceed max_first, while the second may not exceed max_second.
-    For example, range(5, 2,3) would iterate over
-         (1,3), (2,2)
-    which starts at the max_second contraint, and goes to the max_first constraint.
-    If the sum of both maxes is less than the total n, there will be no valid iterator positions. So:
-
-    0 <= a <= A
-    0 <= b <= B
-    0 <= a+b = N
-
-    so 
-
-    0 <= a <= A
-    0 <= N-a <= B
-
-    so
-
-    0 <= a <= A
-    0 >= a-N >= -B
-
-    so
-
-    0 <= a <= A
-    N >= a >= N-B
-
-    so
-
-    0 <= a <= A
-    N-B <= a <= N
-
-    so
-
-    max(0, N-B) <= a <= min(A,N)
-
-    """
-  
-    if n<0 or max_first<0 or max_second<0:
-        # No valid iterator positions can be returned.
-        return
-    
-    for a in range(max(0, n-max_second), min(max_first, n)+1):
-        yield a, n-a
-
 
 def generate_all_useful_vertex_matches(
         M, #number of bad bats
