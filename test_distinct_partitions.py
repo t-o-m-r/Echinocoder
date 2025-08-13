d4aa607d2e5507aeb224048379b8c5c99dc547db
kesterlester <cgl20@cam.ac.uk>
2025-08-13 23:35:27 +0100

    Now we can start partitions anywhere.

--- test_distinct_partitions.py
+++ test_distinct_partitions.py
@@ -1,5 +1,6 @@
 from distinct_partitions_v1_no_start import distinct_partitions_v1_no_start
-from itertools import zip_longest
+from distinct_partitions_with_start import distinct_partitions_with_start
+from itertools import zip_longest, chain
 
 def test_distinct_partitions():
 
@@ -15,14 +16,17 @@ def test_distinct_partitions():
                       ("ae", "mk"),
                       ("ke", "ma"),
                      ), tuple_of_strings_converter, "set",
+                     ("me","ak"),
         ),
         ( "make", (4,0), (
                       ("make", ""), 
                      ), tuple_of_strings_converter, "set",
+                     ("make", ""),
         ),
         ( "make", (0,4), (
                       ("", "make"), 
                      ), tuple_of_strings_converter, "set",
+                      ("", "make"), 
         ),
         ( "happy", (2,3), (
                       ("ha", "ppy"),
@@ -33,6 +37,7 @@ def test_distinct_partitions():
                       ("pp", "hay"),
                       ("py", "hap"),
                      ), tuple_of_strings_converter, "set",
+                      ("hy", "app"),
         ),
         ( "happy", (2,3), (
                       (('h', 'a'), ('p', 'p', 'y')),
@@ -43,6 +48,7 @@ def test_distinct_partitions():
                       (('p', 'p'), ('h', 'a', 'y')),
                       (('p', 'y'), ('h', 'a', 'p')),
                           ), None, "exact",
+                      (('a', 'y'), ('h', 'p', 'p')),
         ),
         ( "Spain", (2,3), (
             (('S', 'p'), ('a', 'i', 'n')),
@@ -56,6 +62,7 @@ def test_distinct_partitions():
             (('a', 'n'), ('S', 'p', 'i')),
             (('i', 'n'), ('S', 'p', 'a')),
                           ), None, "exact",
+            (('p', 'i'), ('S', 'a', 'n')),
         ),
         ( "happy", (2,1,2), (
 (('h', 'a'), ('p',), ('p', 'y')),
@@ -77,6 +84,7 @@ def test_distinct_partitions():
 (('p', 'y'), ('a',), ('h', 'p')),
 (('p', 'y'), ('p',), ('h', 'a')),
                           ), None, "exact",
+(('p', 'p'), ('y',), ('h', 'a')),
         ),
         ( "Spain", (2,1,2), (
 (('S', 'p'), ('a',), ('i', 'n')),
@@ -110,6 +118,7 @@ def test_distinct_partitions():
 (('i', 'n'), ('p',), ('S', 'a')),
 (('i', 'n'), ('a',), ('S', 'p')),
                           ), None, "exact",
+(('p', 'a'), ('n',), ('S', 'i')),
         ),
         ( "Spain", (2,3), (
             (('S', 'p'), ('a', 'i', 'n')),
@@ -123,46 +132,67 @@ def test_distinct_partitions():
             (('a', 'n'), ('S', 'p', 'i')),
             (('i', 'n'), ('S', 'p', 'a')),
                           ), None, "exact",
+            (('S', 'p'), ('a', 'i', 'n')),
         ),
         ( "make", (5,), (
                       ("", "make"), 
                      ), tuple_of_strings_converter, "throws",
+                     None,
         ),
         ( "make", (5,-1), (
                       ("", "make"), 
                      ), tuple_of_strings_converter, "throws",
+                     None,
         ),
         ( "make", (-1), (
                       ("", "make"), 
                      ), tuple_of_strings_converter, "throws",
+                     None,
         ),
         ( "make", "a", (
                       ("", "make"), 
                      ), tuple_of_strings_converter, "throws",
+                     None,
         ),
     ]
 
   
     
-    for data, n, expected, converter, typ in test_programme:
-        print('================')
+    for data, splitting, expected, converter, typ, start in test_programme:
+        print("==============================")
+        print(f"Starting test with data={data}, splitting={splitting}, type={typ}  and start (where used) of {start}.")
+        print("==============================")
         if typ=="throws":
             try:
-               distinct_partitions_v1_no_start(data, n)
+               distinct_partitions_v1_no_start(data, splitting)
                assert True
             except:
                assert False
             continue
 
-        result_exact = distinct_partitions_v1_no_start(data, n)
-        result_list = tuple(distinct_partitions_v1_no_start(data, n))
+        result_exact = distinct_partitions_v1_no_start(data, splitting)
+        result_list = tuple(distinct_partitions_v1_no_start(data, splitting))
         if converter is not None:
             expected = tuple(map( converter, expected ))
 
         if typ=="exact":
-            for i,j in zip_longest(result_exact, expected):
-                print(f"Hoping {i} == {j}")
-                assert i==j
+            start_pos = None
+            for i, (lesters, itertoolss) in enumerate(zip_longest(distinct_partitions_with_start(data, splitting), distinct_partitions_v1_no_start(data, splitting))):
+                print(f"{i}:         {lesters} {'==' if lesters==itertoolss else '!='} {itertoolss}")
+                if lesters == start and start_pos == None:
+                   start_pos = i
+                assert lesters==itertoolss
+            print("Match confirmed!")
+            assert start_pos is not None
+            print(f"Start pos determined to be {start_pos}.")
+            for i, (lesters, itertoolss) in enumerate(zip_longest(chain(iter([None,]*start_pos),distinct_partitions_with_start(data, splitting, start=start)), distinct_partitions_v1_no_start(data, splitting))):
+                if i < start_pos:
+                    print(f"{i}:         {lesters} ...  {itertoolss}")
+                else:
+                    print(f"{i}:         {lesters} {'==' if lesters==itertoolss else '!='} {itertoolss}")
+                    assert lesters==itertoolss
+
+
             continue
 
         if typ=="set":
@@ -173,3 +203,6 @@ def test_distinct_partitions():
             continue
 
         assert False
+
+
+
