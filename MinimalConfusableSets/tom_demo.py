0844e4e2f38ce65a93fdd96f6478e0ad0afb55eb
kesterlester <cgl20@cam.ac.uk>
2025-08-15 11:42:28 +0100

    Factoring out tools

--- MinimalConfusableSets/tom_demo.py
+++ MinimalConfusableSets/tom_demo.py
@@ -1,6 +1,7 @@
 import sympy as sp
 from functools import partial
 from vertex_matches import generate_viable_vertex_match_matrices
+from sympy_tools import strip_zero_rows, some_row_causes_collapse
 
 def demo():
 
@@ -10,42 +11,14 @@ def demo():
     def f(mat: sp.Matrix):
         return sp.shape(mat)[0] <= 5 # True if mat has 4 or fewer rows.
 
-    def strip_zero_rows(M: sp.Matrix):
-        """Return a copy of M with all-zero rows removed."""
-        nonzero_rows = [i for i in range(M.rows) if any(M[i, j] != 0 for j in range(M.cols))]
-        return M[nonzero_rows, :]
-
-    def some_row_causes_collapse(mat: sp.Matrix, k: int):
-            """
-            Return True if there exists a row in M that has:
-            - at least one non-zero entry, and
-            - fewer than k+1 non-zero entries.
-            """
-            k_plus_1 = k+1
-            rows, cols = mat.shape
-            for r in range(rows):
-                nonzero_count = 0
-                for c in range(cols):
-                    if mat[r, c] != 0:
-                        nonzero_count += 1
-                        if nonzero_count == k_plus_1:
-                            # This row is a good row as it has >= k+1 non-zero entries.
-                            break # i.e. stop scanning the columns of this row!
-                # We reached the end of the row, so assess what to do:
-                if 1 <= nonzero_count < k_plus_1:
-                    return True #  This is a bad row! It has a number of non-zero entreis in {1, 2, ... , k}
-                assert nonzero_count==0 or nonzero_count==k_plus_1
-                # This row was good, so try the next row.
-            # We finished trying rows, so if we got here all rows are good!
-            return False
-
     def matrix_is_not_definitely_bad(mat: sp.Matrix, k:int):
         rre, _ = mat.rref()
-        #print("inside mat", mat)
-        #print("inside rre", rre)
         if some_row_causes_collapse(rre, k):
             #print("Some row causes collapse!")
             return False # Matrix is bad!!
+
+        # Let's now strip the zero rows and make the R-rre hashable
+        rre = sp.ImmutableMatrix(strip_zero_rows(rre))
 
         return True # Can't say that matrix is bad, yet!
 
